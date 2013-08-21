99814bca210edd39d80d73e3a9261c7f6d37a7cb
Adrien Thebo <git@somethingsinistral.net>
2013-08-21 13:38:36 -0700

    hack up helpers.

--- nanoc/lib/default.rb
+++ nanoc/lib/default.rb
@@ -6,13 +6,33 @@ $slide_index = 1
 def slide(&block)
   buffer = eval('_erbout', block.binding)
 
-  str = <<-STR
-  <section id="slide-#{$slide_index}" markdown=0>
-    #{yield}
-  </section>
-  STR
+  require 'nanoc/helpers/capturing'
+  extend Nanoc::Helpers::Capturing
 
-  buffer << str
+  c = capture(&block)
+
+  buffer << %[<!-- begin slide #{$slide_index} -->]
+  buffer << %[<section id="slide-#{$slide_index}" markdown="1">]
+  buffer << c
+  buffer << %[</section>\n]
+  buffer << %[<!-- end slide #{$slide_index} -->]
+
+  $slide_index += 1
 
   buffer
+end
+
+def partial(m)
+
+  regex = %r[\A/_#{m}/\Z]
+
+  puts "searching for #{regex}"
+  found = @items.select { |i| i.identifier.match regex }.first
+
+
+  puts "found!" if found
+  require 'pry'
+  binding.pry if found
+
+  found.raw_content
 end
